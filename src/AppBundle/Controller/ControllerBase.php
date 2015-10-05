f0fc20eac30ab17044cae8c238450d9220ff9a11
Martind64 <Martind_64@hotmail.com>
2015-10-05 14:57:07 +0200

    Added ControllerBase and empty EquipmentDataController

---
+++ src/AppBundle/Controller/ControllerBase.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Doctrine\ORM\Mapping as ORM;
+
+
+ControllerBase:
+
+
+class ControllerBase extends Controller
+    {
+    /**
+    * @return EntityManager
+    */
+    public function getEm()
+    {
+        return $this->get('doctrine.orm.default_entity_manager');
+    }
+}
