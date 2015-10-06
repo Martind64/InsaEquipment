d40674c06a9b8b3a4c00bf161968e9c4cb0287ed
Martind64 <Martind_64@hotmail.com>
2015-10-06 10:07:59 +0200

    Added EquipmentDataController test, it's just storing my method atm

---
+++ src/AppBundle/Form/Type/EquipmentDataType.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace AppBundle\Form\Type;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+
+class PersonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add("");
+
+    }
+    public function getName()
+    {
+        return 'EquipmentData';
+    }
+}
+//->add('name', 'text', array('attr' => array('class' => 'form-control')))
+//->add('age', 'integer', array('attr' => array('class' => 'form-control')))
+//->add('save', 'submit', array('attr' => array('class' => 'btn btn-primary btn-lg')));
---
+++ src/AppBundle/Tests/Controller/EquipmentDataControllerTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use AppBundle\Entity\CalibrationInfo;
+use AppBundle\Entity\CalibrationPoint;
+use AppBundle\Entity\EquipmentData;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class EquipmentDataControllerTest extends WebTestCase
+{
+    public function testAddAction()
+    {
+
+        $equipmentData = new EquipmentData();
+        $equipmentData->setDescription('kalibrerer vand instrumenter');
+        $equipmentData->setSerialNr('23153');
+        $equipmentData->setCategory('decade Box');
+        $equipmentData->setDoneBy('Leif Jensen');
+        $equipmentData->setApprovedBy('Lars Christiansen');
+        $equipmentData->setCategory('');
+        $equipmentData->setEquipmentID('KEK31');
+        $equipmentData->setLevel('high level');
+        $equipmentData->setModel('something');
+        $equipmentData->setPartner('n/a');
+        $equipmentData->setPlacement('Jylland');
+
+        $calibrationPoint = new CalibrationPoint();
+        $calibrationPoint->setMeasurement(50);
+        $calibrationPoint->setPrefix('Kilo');
+        $calibrationPoint->setUnit('AAc');
+
+        $calibrationInfo = new CalibrationInfo();
+        $calibrationInfo->setActualUncertainty(1);
+        $calibrationInfo->setAdjustmentLimit(2);
+        $calibrationInfo->setComment('ikke godt nok');
+        $calibrationInfo->setDeviation(0.1);
+        $calibrationInfo->setReferenceNr(123);
+        $calibrationInfo->setUncertaintyRequests('n/a');
+        $calibrationInfo->setUut('n/a');
+        $calibrationInfo->setEquipmentData($equipmentData);
+        $calibrationInfo->setCalibrationPoint($calibrationPoint);
+
+
+        $em =  $this->getEM();
+
+        $em->persist($equipmentData);
+        $em->persist($calibrationInfo);
+        $em->persist($calibrationPoint);
+
+        $em->flush();
+
+        return new Response('The Data was submitted');
+    }
+}
